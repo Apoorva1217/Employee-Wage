f467952fd1ed32a278671514322ebf44d9bc83cc
Apoorva1217 <apoorvarasal12@gmail.com>
2020-10-25 22:52:42 +0530

    [Apoorva] Add . Refactor the code to write a Class Method to compute Employee Wage

--- Employee-Wage/Program.cs
+++ Employee-Wage/Program.cs
@@ -4,26 +4,25 @@ namespace Employee
     class Program
     {
         /// <summary>
-        /// Calculate Wages till a condition of totalworking hours or days is reached for a month
+        /// Refactor the code to write a Class Method to compute Employee Wage
         /// </summary>
         /// <param name="args"></param>
-		
-	//Constants
-	public const int PART_TIME=1;
-	public const int FULL_TIME=2;
-        public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 20;
-        public const int MAX_HRS_IN_MONTH = 100;
-		
-        static void Main(string[] args)
+
+        //Constants
+        const int PART_TIME = 1;
+        const int FULL_TIME = 2;
+        const int EMP_RATE_PER_HOUR = 20;
+        const int NUM_OF_WORKING_DAYS = 20;
+        const int MAX_HRS_IN_MONTH = 100;
+
+        public static int ComputeEmpWage()
         {
-            
-            //Variables
+            //variables
             int empHrs = 0;
             int totalEmpHrs = 0;
             int totalWorkingDays = 0;
             //Computation
-            while (totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
                 Random random = new Random();
                 int empCheck = random.Next(0,3);
@@ -41,10 +40,15 @@ namespace Employee
                 }
                 totalWorkingDays++;
                 totalEmpHrs += empHrs;
-                Console.WriteLine("Day:" + totalWorkingDays+"Employee Hours:"+empHrs);
+                Console.WriteLine("Day:" + totalWorkingDays + "Employee Hours:" + empHrs);
             }
             int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Total Employee Wage:" + totalEmpWage);
+            return totalEmpWage;
+        }
+        public static void Main(String[] args)
+        {
+            ComputeEmpWage();
         }
     }
 }
