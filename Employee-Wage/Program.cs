1fe12c29b173a900d771a44495d83e32186871c3
Apoorva1217 <apoorvarasal12@gmail.com>
2020-10-25 22:58:48 +0530

    [Apoorva] Add . Compute Employee Wage for Multiple Companies

--- Employee-Wage/Program.cs
+++ Employee-Wage/Program.cs
@@ -4,26 +4,24 @@ namespace Employee
     class Program
     {
         /// <summary>
-        /// Refactor the code to write a Class Method to compute Employee Wage
+        /// Compute Employee Wage for Multiple Companies
         /// </summary>
         /// <param name="args"></param>
 
         //Constants
         const int PART_TIME = 1;
         const int FULL_TIME = 2;
-        const int EMP_RATE_PER_HOUR = 20;
-        const int NUM_OF_WORKING_DAYS = 20;
-        const int MAX_HRS_IN_MONTH = 100;
 
-        public static int ComputeEmpWage()
+        public static int ComputeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
         {
             //variables
             int empHrs = 0;
             int totalEmpHrs = 0;
             int totalWorkingDays = 0;
             //Computation
-            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
             {
+	        totalWorkingDays++;
                 Random random = new Random();
                 int empCheck = random.Next(0,3);
                 switch (empCheck)
@@ -38,17 +36,17 @@ namespace Employee
                         empHrs = 0;
                         break;
                 }
-                totalWorkingDays++;
                 totalEmpHrs += empHrs;
                 Console.WriteLine("Day:" + totalWorkingDays + "Employee Hours:" + empHrs);
             }
-            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Total Employee Wage:" + totalEmpWage);
+            int totalEmpWage = totalEmpHrs * empRatePerHour;
+            Console.WriteLine("Total Employee Wage for company:" +company + "is:" +totalEmpWage);
             return totalEmpWage;
         }
-        public static void Main(String[] args)
+        static void Main(String[] args)
         {
-            ComputeEmpWage();
+            ComputeEmpWage("Big Bazaar",20,26,180);
+	    ComputeEmpWage("DMart",23,24,160);
         }
     }
 }
