dc325b6121e5d3922a09d2bd4d7c6f7e61c758e7
Apoorva1217 <apoorvarasal12@gmail.com>
2020-10-25 22:19:00 +0530

    [Apoorva] Add . Welcome message to Employee Wage Computation Program

---
+++ Employee-Wage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Employee_Wage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program!");
+        }
+    }
+}
