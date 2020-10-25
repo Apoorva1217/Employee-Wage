18de9f6265e58f0966507087a9cfc5b0f199a34b
Apoorva1217 <apoorvarasal12@gmail.com>
2020-10-25 23:08:00 +0530

    [Apoorva] Add . Ability to save total wage for each company

--- Employee-Wage/Program.cs
+++ Employee-Wage/Program.cs
@@ -4,49 +4,72 @@ namespace Employee
     class Program
     {
         /// <summary>
-        /// Compute Employee Wage for Multiple Companies
+        /// Ability to save total wage for each company
         /// </summary>
         /// <param name="args"></param>
-
-        //Constants
-        const int PART_TIME = 1;
-        const int FULL_TIME = 2;
-
-        public static int ComputeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+		static void Main(String[] args)
         {
-            //variables
-            int empHrs = 0;
-            int totalEmpHrs = 0;
-            int totalWorkingDays = 0;
-            //Computation
-            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
-            {
-	        totalWorkingDays++;
-                Random random = new Random();
-                int empCheck = random.Next(0,3);
-                switch (empCheck)
-                {
-                    case PART_TIME:
-                        empHrs = 4;
-                        break;
-                    case FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-                totalEmpHrs += empHrs;
-                Console.WriteLine("Day:" + totalWorkingDays + "Employee Hours:" + empHrs);
-            }
-            int totalEmpWage = totalEmpHrs * empRatePerHour;
-            Console.WriteLine("Total Employee Wage for company:" +company + "is:" +totalEmpWage);
-            return totalEmpWage;
+            EmployeeBuilderObject big_bazaar = new EmployeeBuilderObject("Big Bazaar",20,26,180);
+			EmployeeBuilderObject dmart = new EmployeeBuilderObject("Dmart",23,24,160); 
+			big_bazaar.computeEmpWage();
+			Console.WriteLine(big_bazaar.toString());
+			dmart.computeEmpWage();
+			Console.WriteLine(dmart.toString());
+		}
+	}
+}
+
+public class EmployeeBuilderObject
+{
+    //Constants
+    public const int PART_TIME = 1;
+    public const int FULL_TIME = 2;
+
+    private string company;
+    private int empRatePerHour;
+    private int numOfWorkingDays;
+    private int maxHoursPerMonth;
+    private int totalEmpWage;
+
+    public EmployeeBuilderObject(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+    {
+        this.company=company;
+	this.empRatePerHour=empRatePerHour;
+	this.numOfWorkingDays=numOfWorkingDays;
+	this.maxHoursPerMonth=maxHoursPerMonth;
+    }
+    public void computeEmpWage()
+    {
+	//Variables
+	int empHrs=0;
+	int totalEmpHrs=0;
+	int totalWorkingDays=0;
+
+	//Computation
+       	while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
+       	{
+		totalWorkingDays++;
+          	Random random = new Random();
+            	int empCheck = random.Next(0,3);
+            	switch (empCheck)
+            	{
+              		case PART_TIME:
+                    		empHrs = 4;
+                    		break;
+                	case FULL_TIME:
+                    		empHrs = 8;
+                    		break;
+			default:
+                    		empHrs = 0;
+                    		break;
+            	}
+            	totalEmpHrs += empHrs;
+            	Console.WriteLine("Day:" + totalWorkingDays + "Employee Hours:" + empHrs);
         }
-        static void Main(String[] args)
-        {
-            ComputeEmpWage("Big Bazaar",20,26,180);
-	    ComputeEmpWage("DMart",23,24,160);
-        }
+        totalEmpWage = totalEmpHrs * this.empRatePerHour;
+    }
+    public string toString()
+    {
+	return "Total Employee Wage for Company:" + this.company + "is:" + this.totalEmpWage;
     }
 }
