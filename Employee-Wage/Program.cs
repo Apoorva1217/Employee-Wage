d38822bfbfed5891884f298bb805ecc5c044b6ff
Apoorva1217 <apoorvarasal12@gmail.com>
2020-10-25 22:47:15 +0530

    [Apoorva] Add . Calculating Wages for a month

--- Employee-Wage/Program.cs
+++ Employee-Wage/Program.cs
@@ -4,7 +4,7 @@ namespace Employee
     class Program
     {
         /// <summary>
-        /// Switch Case Statement
+        /// Calculating Wages for a month
         /// </summary>
         /// <param name="args"></param>
 		
@@ -12,29 +12,36 @@ namespace Employee
 	public const int PART_TIME=1;
 	public const int FULL_TIME=2;
         public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
 		
         static void Main(string[] args)
         {
             //Variables
             int empHrs = 0;
             int empWage = 0;
-            Random random = new Random();
+            int totalEmpWage = 0;
             //Computation
-            int empCheck = random.Next(0,3);
-            switch (empCheck)
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
             {
-                case PART_TIME:
-                    empHrs = 4;
-                    break;
-                case FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0,3);
+                switch (empCheck)
+                {
+                    case PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Daily Employee Wage is:" + empWage);
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Daily Employee Wage is:" + empWage);
+            Console.WriteLine("Total Employee Wage:" + totalEmpWage);
         }
     }
 }
