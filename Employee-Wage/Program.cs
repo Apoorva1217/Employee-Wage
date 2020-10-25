506afb55b270ceeea4b721f9df8b2bac7ad45e72
Apoorva1217 <apoorvarasal12@gmail.com>
2020-10-25 22:37:47 +0530

    [Apoorva] Add . Check Employee Presence

--- Employee-Wage/Program.cs
+++ Employee-Wage/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
-
-namespace Employee_Wage
+namespace Employee
 {
     class Program
     {
+        /// <summary>
+        /// Check Employee Presence
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program!");
+            //Constant
+            const int FULL_TIME = 1;
+            Random random = new Random();
+            //Variable
+            int empCheck = random.Next(0, 2);
+            if (empCheck == FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present.");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent.");
+            }
         }
     }
 }
